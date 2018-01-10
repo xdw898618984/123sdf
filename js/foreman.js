bad18b830e7b910a9419aa06b022300da3c6a3e8
xdw898618984 <898618984@qq.com>
2018-01-10 21:30:10 +0800

    值班长信息页面编辑js交互

--- js/foreman.js
+++ js/foreman.js
@@ -63,10 +63,7 @@ $(".produce").click(function () {
 
     function show() {
         let mydate = new Date();
-        let str = "" + mydate.getFullYear() + "/";
-        str += (mydate.getMonth() + 1) + "/";
-        str += mydate.getDate() > 9 ? mydate.getDate() : "0" + mydate.getDate() + " ";
-        str += mydate.getHours() > 9 ? mydate.getHours() + ":" : "0" + mydate.getHours() + ":";
+        let str = mydate.getHours() > 9 ? mydate.getHours() + ":" : "0" + mydate.getHours() + ":";
         str += mydate.getMinutes() > 9 ? mydate.getMinutes() : "0" + mydate.getMinutes();
         return str;
     }
@@ -75,10 +72,11 @@ $(".produce").click(function () {
     //故障现象
     let num5 = $(".appearance textarea").val();
     //故障原因
-    let num6 = $("input:radio[name='cause1']").val();
+    var num6;
+    num6 = $("input:radio[name='cause1']:checked").val();
 
     //拨测情况
-    let num7 = $("input:radio[name='status1']").val();
+    let num7 = $("input:radio[name='status1']:checked").val();
     //影响范围
     let num8 = $("input:text[name='scope1']").val();
     //处理情况
@@ -91,20 +89,158 @@ $(".produce").click(function () {
 
     //是否滞后
 
-    let num12= $("input:radio[name='complain1']").val();
+    let num12 = $("input:radio[name='complain1']").val();
 
     //省监控指挥调度人
 
     let num13 = $("input:text[name='monitoring1']").val();
 
     //专业室
-    let num14=[];
-    $("input:checkbox[name='labs1']:checked").each(function(){
-        num14.push($(this).val()); 
+    let num14 = [];
+    $("input:checkbox[name='labs1']:checked").each(function () {
+        num14.push($(this).val());
     });
-    let item = "【新" + num2 + num1 + num3 + num4 + num5 + num6+num7+num8+num9+num10+num11+num12+num13+num14;
-    $(".preview").text(item)
+
+
+    
+   
+    // Array.forEach(function (value, index, num20, num21) {
+    //     //   console.log(num20[index])  
+    //     //   console.log(num21[index])  
+    //     console.log(thisValue)
+    // })
+    // console.log(num20)
+    // console.log(num21)
+    //城市
+    let num15 = $(".city #city").find("option:selected").text();
+    console.log(num15)
+    //广州联系人
+    let num16 = $("input:text[name='city1']").val();
+    console.log(num16)
+    //发布范围
+    let num17 = [];
+    $("input:checkbox[name='publish1']:checked").each(function () {
+        num17.push($(this).val());
+    });
 
 
 
+
+    //故障原因的容错处理
+    let num18 = $("input:text[name='cause1']").val();
+
+    console.log(num18,18)
+    if (num6 == "待确定") {
+        num6 = "待确定"
+    } else {
+        num6 = "其他" + num18;
+    }
+    //拨测情况容错处理num7
+    let num19 = $("input:text[name='status1']").val();
+
+    if (num7 == "无" || num7 == "正常" || num7 == "失败") {
+        num7;
+    } else {
+        num7 = "其他" + num19;
+    }
+
+
+
+
+
+
+
+
+
+
+    if (num3 == "" || num5 == "" || num6 == "其他" || num7 == "其他" || num16 == "") {
+        alert("内容不完整")
+        return false;
+    } else {
+
+        //专业室联系人的方式
+    let num20 = [];
+    $(".remove").each(function () {
+        num20.push($(this).html())
+
+    })
+
+    let num21 = [];
+    $("input:text[name='object']").each(function () {
+        num21.push($(this).val())
+
+    })
+    $.each(num20, function (value, i) {
+        console.log(value, i);
+        if (i == "") {
+            return false;
+        }
+    })
+    let num22 = [];
+    for (let k = 0; k < num20.length; k++) {
+        //专业室联系人的容错处理
+       if(num21[k]==""){
+           alert("内容不完整")
+           return false;
+       }else{
+        num22.push(num20[k]+num21[k])
+       }
+        
+    }
+
+        let item = '【新|' + num1 + '|' + num2 + num3 + '】' + num4 + num5 + "。" + num6 + "。" +"拨测情况:"+ num7 +"。"+ num8 + 
+    "处理情况:"+ num9 +"。"+ "投诉情况，省监控与客服确认，截止"+ num4+num10 +num11 + num12 + num13 + num14 + num22 + num15 + num16 + "特此汇报。温馨提示:内部信息，未经许可切勿私自传播。" + num17;
+
+        $(".preview_again").text(item)
+
+    }
+
+
+})
+
+
+
+
+
+
+//专业室切换checkbox时联系人的变化
+$("input[name='labs1']").bind("change", function () {
+    if ($(this).is(":checked") == true) {
+        var item1 = '<span class="del"><span class="remove"> 省' + $(this).val() + '信息联络人:</span>' +
+            '<input type = "text" name = "object" ></span>'
+        $(".chamber").append(item1)
+    } else {
+        var a = $(this).val();
+        console.log(a)
+        c = "省" + a + "信息联络人";
+        console.log(c);
+        let num = [];
+        $(".remove").each(function (i, index) {
+            num.push($(this).html())
+            // console.log(index, i,$(this).html().indexOf(a))
+            if ($(this).html().indexOf(c) != -1) {
+                $(this).parent().remove()
+            }
+        })
+
+
+    }
+
+
+});
+
+
+//3.审核保存功能,
+$(".save").click(function(){
+let letter=$(".preview_again").html();
+
+ //添加key-value 数据到 sessionStorage
+ localStorage.setItem("demokey", letter);
+ //通过key来获取value
+ var dt = localStorage.getItem("demokey");
+ alert(dt);
+ //清空所有的key-value数据。
+ //sessionStorage.clear();
+
+console.log(dt);
 })
--- js/index.js
+++ js/index.js
@@ -5,45 +5,47 @@
 
 
 
-//添加按钮
+//1.添加按钮新增记录按钮
 function initialize() {
-    function show(){
+    function show() {
         var mydate = new Date();
         var str = "" + mydate.getFullYear() + "/";
-        str += (mydate.getMonth()+1) + "/";
-        str += mydate.getDate()>9? mydate.getDate() : "0"+mydate.getDate()+" ";
-        str +=mydate.getHours()>9?mydate.getHours() +":" :"0"+mydate.getHours()+":";
-        str +=mydate.getMinutes()>9?mydate.getMinutes(): "0"+mydate.getMinutes();
+        str += (mydate.getMonth() + 1) + "/";
+        str += mydate.getDate() > 9 ? mydate.getDate() : "0" + mydate.getDate() + "/";
+        str += mydate.getHours() > 9 ? mydate.getHours() + ":" : "0" + mydate.getHours() + ":";
+        str += mydate.getMinutes() > 9 ? mydate.getMinutes() : "0" + mydate.getMinutes();
         return str;
-       }
- 
+    }
+
     $("#add").click(function () {
-        var dateNow=show();
+        var dateNow = show();
         var setmp = '<li  class="setmp"  style="position: relative">' +
-            '<input type="checkbox" style="float: left;position: absolute; top: 22px; left:34px;">'+
-            '<div class="click"> '+
-            '<span class="num1">'+show()+'</span>' +
+            '<input type="checkbox" name="cut" style="float: left;position: absolute; top: 22px; left:34px;">' +
+            '<div class="click"> ' +
+            '<span class="num1">' + show() + '</span>' +
             ' <span class="num2">内部通报</span>' +
             ' <span class="num3">待定待定待定待定待定待定待定</span>' +
-            '<span class="num4">'+
-                '<a class="num"href="./foreman.html" target="_blank">新</a>'+
-                '<a class="num"href="./foreman.html" target="_blank">升级</a>'+
-                '<a class="num"href="./foreman.html" target="_blank">阶段</a>'+
-                '<a href="./foreman.html" target="_blank">销毁</a>'+
+            '<span class="num4">' +
+            '<a class="num"href="./foreman.html" target="_blank">新</a>' +
+            '<a class="num"href="./foreman.html" target="_blank">升级</a>' +
+            '<a class="num"href="./foreman.html" target="_blank">阶段</a>' +
+            '<a href="./foreman.html" target="_blank">销毁</a>' +
             '</span> ' +
-            '<span class="num5">'+
-                '<a href="./post.html" target="_blank">大无线</a>'+
-                '<a href="./post.html" target="_blank">大核心</a>'+
+            '<span class="num5">' +
+            '<a href="./post.html" target="_blank">大无线</a>' +
+            '<a href="./post.html" target="_blank">大核心</a>' +
 
             '</span> ' +
-            '<span class="num6">'+
-            '<i class="delete"></i>'+
+            '<span class="num6">' +
 
-            '<!--<a href="./post.html" target="_blank">删除</a>-->'+
-           '<p>删除</p>'+
+
+            '<i class="delete" id="delete"></i>' +
+
+
+            '<p class="deleteTo">删除</p>' +
 
             '</span> ' +
-            '</div>'+
+            '</div>' +
             '</li>';
         $(".results-ul").append(setmp)
 
@@ -52,20 +54,64 @@ function initialize() {
 
 }
 initialize();
-//点击全选，check和背景颜色
+//2.点击全选，check和背景颜色
 $("#down").click(function () {
-$(".results-ul li .click").css({
-    background:"#defd78",
-    borderRadius:10+"px",
-    overflow:"hidden",
-})
-    var input=$("input")
+    $(".results-ul li .click").css({
+        background: "#defd78",
+        borderRadius: 10 + "px",
+        overflow: "hidden",
+    })
+    var input = $("input")
     for (var i = 0; i < input.length; i++) {
         input[i].checked = true;
     }
 })
 
-//搜索按钮
+
+//全部删除，删除全部数据
+
+
+
+
+//4.单选按钮，改变背景颜色
+
+$(document).on("change", "input:checkbox[name='cut']", function () {
+  
+    console.log($(this) )
+    console.log($(this).checked )
+    if ($(this).checked = true) {
+        alert("true")
+    }
+    else{
+        alert("false")
+    }
+})
+
+//5.删除按钮，删除本行数据
+$(document).on("click", ".num6", function () {
+    //一直找到li标签的位置。
+    $(this).parent().parent().remove();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//6.搜索按钮
 function search() {
     var a;
     $("#date").click(function () {
@@ -94,19 +140,25 @@ function search() {
             '<span class="num5"><a href="./post.html" target="_blank">岗位</a></span> ' +
             '</li>';
         $(".results-ul").append(setmp)
+        //清除输入框
+
+        $("#content").val("");
 
     })
-    content = ""; //请求之后让内容重新为空；
+
 }
 
 search();
 
-function getData(url,scope,content) {
+
+
+
+function getData(url, scope, content) {
     $.ajax({
         url: url,
         data: {
-             "范围":scope,
-             "内容":content
+            "范围": scope,
+            "内容": content
 
         },
         success: function (res) {
